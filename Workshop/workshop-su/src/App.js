a1ed28781b2e50849bc7f848afb0fb7cc2a9c202
MiroZe <miro_sv@yahoo.com>
2023-10-16 16:44:04 +0300

    worshop SoftUni init and components buid

---
+++ Workshop/workshop-su/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import { Userlist } from "./components/Userlist";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+          <Userlist />
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
